60871eff33223eda4e3bdf01ef36b77e645e4046
Rhombus14 <jada.ballentine@freescale.com>
2014-04-24 14:11:18 -0500

    TEST 2
    stuff

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,23 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+
+## 1st FUNCTION:
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
-## HOWDY
-
+## 2nd FUNCTION:
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  ## Don't need to worry about a case where matrix is NOT invertible
+#   solve(X) finds the inverse of a square matrix
+  
+  
+#   HI World!
+  
 }
